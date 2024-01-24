e689e411e0ce298e62c991f55a02af19c4e7e82a
Kayla Hunt <huntkay1@msu.edu>
2024-01-24 17:48:19 -0500

    Create makeGrid function
    This function currently creates the correct amount of squares based on
    the number given. This will need to be styled to create a proper grid.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
+const container = document.getElementById("container");
 
+function makeGrid(size) {
+    let gridSize = size * size;
+
+    for (let i=0; i < gridSize; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add('square');
+
+        let gridWidth = container.clientWidth / size + `px`;
+        let gridHeight = container.clientHeight / size + `px`;
+
+        gridSquare.style.width = `${gridWidth}`;
+        gridSquare.style.height = `${gridWidth}`;
+        gridSquare.style.border = "1px green solid";
+
+        container.appendChild(gridSquare);
+    }
+}
+
+makeGrid(10);
