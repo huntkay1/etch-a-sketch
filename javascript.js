29d3f29a2ff10e265dc433e230f2848e12908126
Kayla Hunt <huntkay1@msu.edu>
2024-01-24 19:16:30 -0500

    Fix performance speed issues
    Add a fragment in the getGrid function. This will append the squares to
    a fragment, holding them in memory until they are all ready to be
    appended to the grid container. This improved the speed of the program.

--- javascript.js
+++ javascript.js
@@ -3,12 +3,13 @@ const container = document.getElementById("grid");
 //Creates the boxes used to build the grid based on the specificed dimensions 
 function makeGrid(size) {
     let gridSize = size * size;
+    let fragment = document.createDocumentFragment(); //holds the squares to help performance
 
     for (let i=0; i < gridSize; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add('square');
 
-        //clientHeight includes the padding and border in the container dimensions for proper calcuation
+        //clientHeight includes the padding and border of the container to calculate proper square dimensions
         let squareWidth = container.clientHeight / size + `px`; 
         let squareHeight = container.clientWidth / size + `px`;
 
@@ -16,8 +17,9 @@ function makeGrid(size) {
         gridSquare.style.height = `${squareHeight}`;
         
 
-        container.appendChild(gridSquare);
+        fragment.appendChild(gridSquare);
     }
+
+    container.appendChild(fragment); //adds the squares 
 }
 
-makeGrid(20);
