b3164615b9f2efff1c3fa82cee15f08687c20d64
Kayla Hunt <huntkay1@msu.edu>
2024-01-26 17:55:22 -0500

    Create rainbow mode
    Added a button that when clicked, turns on rainbow mode. This mode
    selects a randomized color for each square when hovered over. This mode
    will be turned off when clicked again - reverting to classic mode.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 let eraseMode = false;
+let rainbowMode = false;
 
 //Creates the boxes used to build the grid based on the specificed dimensions 
 function makeGrid(size) {
@@ -30,6 +31,7 @@ function makeGrid(size) {
     container.appendChild(fragment); //adds the squares fragment 
 }
 
+
 //changes the color of the square when moused over
 function changeColor() {
 
@@ -44,7 +46,8 @@ function changeColor() {
 
 }
 
-//initializes the grid so that it first displays with 16 squares. Same function as makeGrid
+
+//Only used for inital display of grid - creates 16x16 grid with white background
 function initializeGrid() {
     let size = 16;
     let gridSize = size * size;
@@ -82,7 +85,6 @@ sizeBtn.textContent = "Select Size";
 sizeBtn.classList.add("select");
 btnContainer.appendChild(sizeBtn);
 
-
 //allows user to choose dimensions
 sizeBtn.addEventListener('click', () => {
     gridSize = prompt("Please specify a dimension number: "); 
@@ -114,16 +116,19 @@ clearBtn.addEventListener('click', () => {
 
 })
 
+
+//creates erase button
 eraseBtn = document.createElement("button");
 eraseBtn.textContent = "Erase";
 eraseBtn.classList.add("erase");
 btnContainer.appendChild(eraseBtn);
 
+//turn on the erase function when clicked and turn it off when clicked again
 eraseBtn.addEventListener('click', () => { 
     eraseMode = !eraseMode;
 
     if (eraseMode) {
-        eraseBtn.style.backgroundColor = "gray";
+        eraseBtn.textContent = "Draw";
         let squares = document.getElementsByClassName('square');
         let squareArray = [...squares]; //change to array
 
@@ -133,7 +138,7 @@ eraseBtn.addEventListener('click', () => {
             });
         } )
     } else {
-        eraseBtn.style.backgroundColor = "transparent";
+        eraseBtn.textContent = "Erase";
         let squares = document.getElementsByClassName('square');
         let squareArray = [...squares]; //change to array
 
@@ -143,9 +148,42 @@ eraseBtn.addEventListener('click', () => {
             });
         } )
     }
-
-    
 })
+
+
+//create rainbow mode button
+rainbowBtn = document.createElement("button");
+rainbowBtn.textContent = "Rainbow Mode";
+rainbowBtn.classList.add("rainbow");
+btnContainer.appendChild(rainbowBtn);
+
+//Turns on rainbow mode when clicked, choosing a random color each time a square
+//is moused over. Turns off when clicked again
+rainbowBtn.addEventListener('click', () => {
+    rainbowMode = !rainbowMode;
+    let squares = document.getElementsByClassName('square');
+    let squareArray = [...squares]; //change to array
+
+    if (rainbowMode) {
+        rainbowBtn.textContent = "Classic Mode";
+        squareArray.forEach((square) => {
+            square.addEventListener('mouseenter', () => {
+                var letters = '0123456789ABCDEF';
+                var color = '#';
+                for (var i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                square.style.backgroundColor = color;
+            });
+        } )
+
+    } else {
+        rainbowBtn.textContent = "Rainbow Mode";
+        changeColor();
+    }
+})
+
+
 
 initializeGrid();
 changeColor();
